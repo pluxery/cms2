5a7319ff8d3f1a32bfacccd07dd0d333988db932
pluxery <84563687+pluxery@users.noreply.github.com>
2022-01-24 06:07:46 +1000

    fix

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const config = require('config')
 const mongoose = require('mongoose')
 const pizzaRouter = require("./routes/pizzaRouter");
 
@@ -11,15 +10,16 @@ app.use(express.json({ extended: true }))
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/pizza', pizzaRouter)
 
-const PORT = config.get('port') || 5000
+const PORT = 5000
+const mongoUrl ='mongodb+srv://sema:123@cluster0.2hlcl.mongodb.net/Cluster0?retryWrites=true&w=majority'
 
 async function start() {
     try {
-        await mongoose.connect(config.get('mongoUrl'), {
+        await mongoose.connect(mongoUrl), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
-        })
+        }
         app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
     } catch (e) {
         console.log('Server Error', e.message)
