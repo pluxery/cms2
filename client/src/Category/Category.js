4b3ae39b9a671c9fe79948e9d573def1411f7a49
pluxery <93857847+lovikbobik@users.noreply.github.com>
2022-01-25 07:35:02 +1000

    update Category.js

--- client/src/Category/Category.js
+++ client/src/Category/Category.js
@@ -1,16 +1,18 @@
 import React from "react";
 import './Category.css'
 import CategoryBox from "./CategoryBox";
-import {NavLink} from "react-router-dom";
+
 
 function Category({category}) {
     return (
         <div className={'category'}>
 
             <div className={'category-row'}>
-                <CategoryBox image={category[0].image} category={category[0].name} link={category[0].link}/>
-                <CategoryBox image={category[1].image} category={category[1].name} link={category[1].link}/>
-                <CategoryBox image={category[2].image} category={category[2].name} link={category[2].link}/>
+
+                {category.map(item => (
+                    <CategoryBox image={item.image} category={item.name} link={item.link}/>
+                ))}
+
             </div>
         </div>
     );
--- client/src/Data/categoryDessert.js
+++ client/src/Data/categoryDessert.js
@@ -1,15 +1,18 @@
 let categoryDessert = [
     {
-        image:'https://s82079.cdn.ngenix.net/330x0/wjqq9ij12k6242u1vo17febayrin',
-        name:'Мороженное'
+        image: 'https://s82079.cdn.ngenix.net/330x0/wjqq9ij12k6242u1vo17febayrin',
+        name: 'Мороженное',
+        link: "/desserts/ice-cream"
     },
     {
-        image:'https://s82079.cdn.ngenix.net/330x0/H7zG8BhzscDEjpudqHH1eU8J.png',
-        name:'Кексы'
+        image: 'https://s82079.cdn.ngenix.net/330x0/H7zG8BhzscDEjpudqHH1eU8J.png',
+        name: 'Кексы',
+        link: "/desserts/gingerbread"
     },
     {
         image: 'https://s82079.cdn.ngenix.net/330x0/CDjH58C3YsVcXuoMUco4fa4t.png',
-        name: 'Пироженное'
+        name: 'Пироженное',
+        link: "/desserts/cake"
     }
 ]
 
