1def4a67449462e1bd81c0dd88e82279cf40ee41
lovikbobik <afanaseva.sd@students.dvfu.ru>
2022-01-25 07:49:11 +0300

    front dessert

--- client/src/Data/categoryDessert.js
+++ client/src/Data/categoryDessert.js
@@ -5,13 +5,13 @@ let categoryDessert = [
         link: "/desserts/ice-cream"
     },
     {
-        image: 'https://s82079.cdn.ngenix.net/330x0/H7zG8BhzscDEjpudqHH1eU8J.png',
-        name: 'Кексы',
+        image: 'https://dodopizza-a.akamaihd.net/static/Img/Products/e168f60e82014ac8947d7b8bf8a53eaf_366x366.jpeg',
+        name: 'Пончики',
         link: "/desserts/gingerbread"
     },
     {
-        image: 'https://s82079.cdn.ngenix.net/330x0/CDjH58C3YsVcXuoMUco4fa4t.png',
-        name: 'Пироженное',
+        image: 'https://dodopizza-a.akamaihd.net/static/Img/Products/6eacb241458547c68a570d86954568aa_366x366.jpeg',
+        name: 'Пирожное',
         link: "/desserts/cake"
     }
 ]
